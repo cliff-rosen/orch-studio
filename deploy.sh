6630346ba7e004d8f398f02dd74594e39766e7a7
Cliff Rosen <cliff.rosen@gmail.com>
2026-03-19 07:00:33 -0600

    'b'

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+BUCKET="s3://orchestratorstudios.com"
+DIST_ID="E3IQ5B4FKKJX25"
+
+echo "Deploying to $BUCKET..."
+
+# Upload HTML files
+aws s3 cp index.html "$BUCKET/index.html" --content-type "text/html"
+aws s3 sync articles/ "$BUCKET/articles/" --content-type "text/html"
+
+# Upload assets
+aws s3 cp favicon.svg "$BUCKET/favicon.svg" --content-type "image/svg+xml"
+
+echo "Invalidating CloudFront cache..."
+aws cloudfront create-invalidation --distribution-id "$DIST_ID" --paths "/*" --query "Invalidation.Id" --output text
+
+echo "Done. Changes will be live in ~1-2 minutes."
